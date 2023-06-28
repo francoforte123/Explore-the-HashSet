96a6a8834d9e4569e85f60056042c0d8021c967d
rober <rober@DESKTOP-MN9AKPU>
2023-06-28 08:27:48 +0200

    Aggiungere i file di progetto.

---
+++ Explore the HashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> ListOfNames= new List<string>();
+HashSet<string> HashsetListOfName= new HashSet<string>();
+
+ListOfNames.Add("Anna");
+ListOfNames.Add("Claudia");
+ListOfNames.Add("Marco");
+ListOfNames.Add("Luca");
+ListOfNames.Add("anna");
+ListOfNames.Add("luca");
+ListOfNames.Add("Marco");
+
+Console.WriteLine("sto stampando i nomi della lista:");
+
+foreach(string name in ListOfNames)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("\nsto aggiungendo i nomi della ListOfNames all'HashsetListOfName, per vedere cosa accade");
+
+foreach (string names in ListOfNames)
+{
+    HashsetListOfName.Add(names);
+}
+
+Console.WriteLine("\nsto stampando i nomi dell'HashsetListOfName");
+
+foreach (string names in HashsetListOfName)
+{
+    Console.WriteLine(names);
+}
+
+
+
+
